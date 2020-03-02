9ae925dc02e07e8e7a9006779afcd721cfe56138
KoreanGinseng <49602459+KoreanGinseng@users.noreply.github.com>
2020-03-02 11:27:49 +0900

    テストコード削除

--- Project/GameApp.cpp
+++ Project/GameApp.cpp
@@ -11,14 +11,6 @@
 #include	"GameApp.h"
 #include	"ToolControll.h"
 
-CButton		gBtn;
-CLabel		gLbl;
-CCheckBox	gCB;
-CScrollBar  gSB;
-CListBox	gLB;
-CTextBox    gTB1;
-CTextBox    gTB2;
-
 /*************************************************************************//*!
 		@brief			アプリケーションの初期化
 		@param			None
@@ -27,66 +19,7 @@ CTextBox    gTB2;
 						それ以外	失敗、エラーコードが戻り値となる
 *//**************************************************************************/
 MofBool CGameApp::Initialize(void) {
-	CControll::CreateInfo bc = {
-		Vector2(100,100),
-		Vector2(100,100),
-		"btn"
-	};
-	CControll::CreateInfo lc = {
-		Vector2(400,100),
-		Vector2(100,24),
-		"lbl"
-	};
-	CControll::CreateInfo cbc = {
-		Vector2(400,200),
-		Vector2(150,24),
-		"CheckBox"
-	};
-	CControll::CreateInfo sbc = {
-		Vector2(600,100),
-		Vector2(150,500),
-		""
-	};
-	CControll::CreateInfo lbc = {
-		Vector2(600,100),
-		Vector2(150,500),
-		""
-	};
-	CControll::CreateInfo tbc1 = {
-		Vector2(100,300),
-		Vector2(150,24),
-		""
-	};
-	CControll::CreateInfo tbc2 = {
-		Vector2(100,400),
-		Vector2(150,24),
-		""
-	};
-	gBtn.FontCreate(24, "");
-	gBtn.SetInfo(bc);
-	gLbl.FontCreate(24, "");
-	gLbl.SetInfo(lc);
-	gCB.FontCreate(24, "");
-	gCB.SetInfo(cbc);
-	
-	gSB.FontCreate(24, "");
-	gSB.SetInfo(sbc);
-	gSB.SetWidth(20);
-	gSB.SetScrlArea(gSB.GetRect(), 10000);
-	gSB.Initialize();
-	gSB.SetShow(FALSE);
-
-	gLB.SetInfo(lbc);
-	gLB.Initialize();
-
-	gTB1.FontCreate(24,"");
-	gTB2.FontCreate(24,"");
-	gTB1.SetInfo(tbc1);
-	gTB2.SetInfo(tbc2);
-	gTB1.Initialize();
-	gTB2.Initialize();
-	gTB2.SetNumOnly(TRUE);
-
+	CUtilities::SetCurrentDirectory("Resource");
 
 	return TRUE;
 }
@@ -102,55 +35,6 @@ MofBool CGameApp::Update(void) {
 	g_pInput->RefreshKey();
 	CTextBox::RefreshImeInput();
 
-	gCB.Update();
-	gSB.Update();
-	gTB1.Update();
-	gTB2.Update();
-
-	if (g_pInput->IsKeyPush(MOFKEY_1))
-	{
-		gSB.ChangeMaxSize(1000);
-	}
-	if (g_pInput->IsKeyPush(MOFKEY_2))
-	{
-		gSB.ChangeMaxSize(100000);
-	}
-	if (g_pInput->IsKeyPush(MOFKEY_3))
-	{
-		gLB.Add();
-	}
-	if (g_pInput->IsKeyPush(MOFKEY_4))
-	{
-		gLB.Delete();
-	}
-	if (g_pInput->IsKeyPush(MOFKEY_5))
-	{
-		gLB.DeleteFast();
-	}
-	if (g_pInput->IsKeyPush(MOFKEY_6))
-	{
-		gLB.AddFast("str");
-		gLB.AddLast("rts");
-	}
-	if (g_pInput->IsKeyPush(MOFKEY_7))
-	{
-		int idx = gLB.GetSelectNo();
-		gLB.Swap(idx, idx + 1);
-	}
-	if (gBtn.IsPush())
-	{
-		gTB2.AddNum(10);
-	}
-	if (gBtn.IsHold())
-	{
-		gTB2.AddNum(0.2f);
-	}
-	if (gBtn.IsPull())
-	{
-		gTB2.SubNum(10);
-	}
-
-	gLB.Update();
 	
 	return TRUE;
 }
@@ -167,13 +51,6 @@ MofBool CGameApp::Render(void) {
 	//画面のクリア
 	g_pGraphics->ClearTarget(0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0);
 
-	gBtn.Render();
-	gLbl.Render();
-	gCB.Render();
-	gSB.Render();
-	gLB.Render();
-	gTB1.Render();
-	gTB2.Render();
 	//描画の終了
 	g_pGraphics->RenderEnd();
 	return TRUE;
@@ -186,8 +63,6 @@ MofBool CGameApp::Render(void) {
 						それ以外	失敗、エラーコードが戻り値となる
 *//**************************************************************************/
 MofBool CGameApp::Release(void) {
-	gSB.Release();
-	gLB.Release();
 	CTextBox::ReleaseList();
 	return TRUE;
 }
